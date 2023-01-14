01f6ad3400c5fbcdd4835f88e8fc94b6b773f94b
RamrajCh <ramrajchimouriya@gmail.com>
2023-01-14 16:49:23 +0545

    the minions game

--- the-minions-game/the-minions-game.py
+++ the-minions-game/the-minions-game.py
@@ -1,17 +1,18 @@
 def minion_game(S):
-    s=0
-    k=0
+    s = 0
+    k = 0
     for i in range(len(S)):
-        if S[i]=='A' or S[i]=='E' or S[i]=='I' or S[i]=='O' or S[i]=='U':
-            k+=len(S)-i
+        if S[i] == 'A' or S[i] == 'E' or S[i] == 'I' or S[i] == 'O' or S[i] == 'U':
+            k += len(S)-i
         else:
-            s+=len(S)-i
-    if s==k:
+            s += len(S)-i
+    if s == k:
         print("Draw")
-    elif k>s:
-        print("Kevin "+ str(k))
+    elif k > s:
+        print("Kevin " + str(k))
     else:
-        print("Stuart "+ str(s))
+        print("Stuart " + str(s))
+
 
 if __name__ == '__main__':
     s = input()
