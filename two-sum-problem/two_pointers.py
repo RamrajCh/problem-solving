584169b17d0b9964f0b4010a630f0b8e3a107efb
RamrajCh <ramrajchimouriya@gmail.com>
2023-01-16 11:29:10 +0545

    two_sum_problem two_pointers

--- two-sum-problem/two_pointers.py
+++ two-sum-problem/two_pointers.py
@@ -1,13 +1,11 @@
-def main(L, sum):
+def main(L, target):
     start = 0
     end = len(L) - 1
     
-    while True:
-        if start == end:
-            break
-        if L[start] + L[end] == sum:
+    while start < end:
+        if L[start] + L[end] == target:
             return (L[start], L[end])
-        elif L[start] + L[end] < sum:
+        elif L[start] + L[end] < target:
             start += 1
         else:
             end -= 1
@@ -19,12 +17,12 @@ if __name__ == "__main__":
     # Input: [1, 2, 3, 4, 5], 5
     # standard_input = "[2,1,3,6,4,5],5"
     S = input()
-    L, sum = S.split("],")
+    L, target = S.split("],")
     L = list(map(lambda x:int(x), L[1:].split(",")))
     # sort the list
     L.sort()
     
-    two_sum_solution = main(L, int(sum))
+    two_sum_solution = main(L, int(target))
     
     print(f"Input: {S}")
     if two_sum_solution:
